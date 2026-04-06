4d9366b7aaade96fc73dccf1b4559acb507ce40e
IhsanAlhakim <ihsanalhakim04@gmail.com>
2026-04-06 15:07:44 +0700

    fix(custom mux): ensure middleware executes in registration order

--- internal/mux/mux.go
+++ internal/mux/mux.go
@@ -1,6 +1,8 @@
 package mux
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type Mux struct {
 	http.ServeMux
@@ -14,8 +16,20 @@ func (m *Mux) RegisterMiddleware(next func(next http.Handler) http.Handler) {
 func (m *Mux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var current http.Handler = &m.ServeMux
 
-	for _, next := range m.middlewares {
-		current = next(current)
+	/*
+		middleware paling awal di slice adalah middleware yg membungkus pertama handler
+		jadi ketika register middleware 1 baru 2
+		yg dijalanin yg kedua dulu karna paling luar
+	*/
+	// for i, next := range m.middlewares {
+	// 	current = next(current)
+	// }
+
+	/*
+		kalau mau yg registrasi pertama dijalanin pertama
+	*/
+	for i := len(m.middlewares) - 1; i >= 0; i-- {
+		current = m.middlewares[i](current)
 	}
 
 	current.ServeHTTP(w, r)
